899f3a46695f974c1f0c84a2f1dba0d4762f7a1d
Ariel Khawly <akhawly59@gmail.com>
2020-02-27 13:44:18 -0800

    switch case for inquirer and beginning adding command lines

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
+const inquirer = require('inquirer');
 
 let app = express();
 
@@ -18,29 +19,72 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-
+  runOffice();
   console.log("connected as id " + connection.threadId);
 });
 
-// route time
-// route for adding into departments
+// route time !!
+function runOffice() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a department, job, or employee",
+        "View department, job, or employee",
+        "Update department, job, or employee",
+        "exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add a department, job, or employee":
+        addOffice();
+        break;
+
+      case "View department, job, or employee":
+        viewOffice();
+        break;
+
+      case "Update department, job, or employee":
+        updateOffice();
+        break;
+
+      case "exit":
+        connection.end();
+        break;
+      }
+    });
+}
+
+// all routes for adding, will be set w/i inquirer 
+//route for adding into department
+function addOffice() {
 app.get('/', function(req, res){
     if (err) throw err;
     connection.query("INSERT INTO departments (depart_name) VALUES ?")
 });
+runOffice();
+}
 // route for adding into employee
-app.get('/', function(req, res){
-  if (err) throw err;
-  connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ?")
+function viewOffice() {
+  app.get('/', function(req, res){
+    if (err) throw err;
+    connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ?")
+  
+  });
+  runOffice();
+}
 
-});
 //route for adding into job
-app.get('/', function(req, res){
-  if (err) throw err;
-  connection.query("INSERT INTO job (title, salary, department_id) VALUES ?")
-});
-//route for updating
-
+function updateOffice() {
+  app.get('/', function(req, res){
+    if (err) throw err;
+    connection.query("INSERT INTO job (title, salary, department_id) VALUES ?")
+  });
+  runOffice();
+}
 
 
 // Start our server so that it can begin listening to client requests.
