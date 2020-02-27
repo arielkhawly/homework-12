b3e1394009bbd3569ea2fd01b9353143ffa18e1c
Ariel Khawly <akhawly59@gmail.com>
2020-02-27 13:15:01 -0800

    routes for adding

--- app.js
+++ app.js
@@ -22,6 +22,27 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// route time
+// route for adding into departments
+app.get('/', function(req, res){
+    if (err) throw err;
+    connection.query("INSERT INTO departments (depart_name) VALUES ?")
+});
+// route for adding into employee
+app.get('/', function(req, res){
+  if (err) throw err;
+  connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ?")
+
+});
+//route for adding into job
+app.get('/', function(req, res){
+  if (err) throw err;
+  connection.query("INSERT INTO job (title, salary, department_id) VALUES ?")
+});
+//route for updating
+
+
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
